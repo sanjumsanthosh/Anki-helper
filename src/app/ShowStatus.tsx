5ee2ba7a0c295460650a0a19670f336b2899f589
sanjay <44315700+sanjumsanthosh@users.noreply.github.com>
2024-04-20 00:15:17 +0530

    feat: Add provider component to root layout
    This commit adds the `Provider` component to the root layout in `src/app/layout.tsx`. The `Provider` component is imported from `@/components/provider` and is used to provide a theme context to the entire application.
    
    The `Provider` component is wrapped around the existing `children` in the `RootLayout` function. It sets the default theme to "system" and enables the system theme. The `main` element inside the `body` is given a specific class name for styling purposes.
    
    This change improves the theming capabilities of the application and allows for easy customization of the theme throughout the entire app.

---
+++ src/app/ShowStatus.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function ShowStatus() {
+    return (
+        <div className="flex flex-col">
+        <Button variant="brand" size="xl">
+            Get Started
+        </Button>
+        </div>
+    );
+}
